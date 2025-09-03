849999174bb25cd1f1b3ca44bdd3f87d6a5a8d5f
pradeep-glitch <pradeepskuri@gmail.com>
2025-09-04 00:12:51 +0530

    recipe total 25 create test case added

--- app/core/models.py
+++ app/core/models.py
@@ -43,7 +43,7 @@ class Recipe(models.Model):
         on_delete=models.CASCADE,
     )
 
-    title = models.CharField(max_length=255),
+    title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     time_minutes = models.IntegerField()
     price = models.DecimalField(max_digits=5,decimal_places=2)
